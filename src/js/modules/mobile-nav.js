2a00c122c8a5d01ba0b56a437e4ab45e7bb73a0c
Roman <bezuglijj7@gmail.com>
2024-01-18 23:07:02 +0200

    changes header funcional

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -14,28 +14,14 @@ document.addEventListener("DOMContentLoaded", function () {
     body.classList.remove("no-scroll");
   });
 
-  // Get all navigation links
-  const navLinks = document.querySelectorAll(".header__nav a");
-
-  // Function to handle click event
-  function handleNavLinkClick(event) {
-    // Remove 'active' class from all links
-    navLinks.forEach((link) => link.classList.remove("active"));
-
-    // Add 'active' class to the clicked link
-    event.target.classList.add("active");
-  }
-
-  // Attach click event listener to each navigation link
-  navLinks.forEach((link) => {
-    link.addEventListener("click", handleNavLinkClick);
-  });
-
   // Mobile navigation links
   const mobileNavLinks = document.querySelectorAll(".header-mobile__nav a");
 
   // Attach click event listener to each mobile navigation link
   mobileNavLinks.forEach((link) => {
-    link.addEventListener("click", handleNavLinkClick);
+    link.addEventListener("click", function () {
+      mobileMenu.classList.remove("open");
+      body.classList.remove("no-scroll");
+    });
   });
 });
