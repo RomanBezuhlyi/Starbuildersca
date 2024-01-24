4444300a14574a126e6e9db596a84511ea4d5b3c
Roman <bezuglijj7@gmail.com>
2024-01-24 14:42:24 +0200

    added btn

--- src/js/modules/gallery.js
+++ src/js/modules/gallery.js
@@ -17,15 +17,99 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-var swiper = new Swiper(".gallery__slider", {
+var swiper = new Swiper(".gallery__slider-1", {
   navigation: {
-    prevEl: ".gallery__slider-prev",
-    nextEl: ".gallery__slider-next",
+    prevEl: ".gallery__slider-prev-1",
+    nextEl: ".gallery__slider-next-1",
   },
   slidesPerView: 1.5,
   initialSlide: 2,
   spaceBetween: 7,
-  loop: true,
+  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var swiper = new Swiper(".gallery__slider-2", {
+  navigation: {
+    prevEl: ".gallery__slider-prev-2",
+    nextEl: ".gallery__slider-next-2",
+  },
+  slidesPerView: 1.5,
+  initialSlide: 2,
+  spaceBetween: 7,
+  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var swiper = new Swiper(".gallery__slider-3", {
+  navigation: {
+    prevEl: ".gallery__slider-prev-3",
+    nextEl: ".gallery__slider-next-3",
+  },
+  slidesPerView: 1.5,
+  initialSlide: 2,
+  spaceBetween: 7,
+  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var swiper = new Swiper(".gallery__slider-4", {
+  navigation: {
+    prevEl: ".gallery__slider-prev-4",
+    nextEl: ".gallery__slider-next-4",
+  },
+  slidesPerView: 1.5,
+  initialSlide: 2,
+  spaceBetween: 7,
+  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var swiper = new Swiper(".gallery__slider-5", {
+  navigation: {
+    prevEl: ".gallery__slider-prev-5",
+    nextEl: ".gallery__slider-next-5",
+  },
+  slidesPerView: 1.5,
+  initialSlide: 2,
+  spaceBetween: 7,
+  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var swiper = new Swiper(".gallery__slider-6", {
+  navigation: {
+    prevEl: ".gallery__slider-prev-6",
+    nextEl: ".gallery__slider-next-6",
+  },
+  slidesPerView: 1.5,
+  initialSlide: 2,
+  spaceBetween: 7,
   centeredSlides: true,
   breakpoints: {
     768: {
